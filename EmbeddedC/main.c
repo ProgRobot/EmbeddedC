ce9cec2abb01bcac34d22b61fe2752812042bd42
YoucefKF <kfyoucef@gmail.com>
2021-09-17 11:25:10 +0200

    Add project files.

---
+++ EmbeddedC/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>;
+#include <stdlib.h>;
+
+int main(int argc, char* argv[]) {
+	
+	int age = 0; // On initialise la variable à 0
+
+	printf("Quel age avez-vous ? ");
+	scanf_s("%d", &age); 
+	// On demande d'entrer l'âge avec scanf
+	printf("Ah ! Vous avez donc %d ans !\n\n", age);
+	return 0;
+
+}
