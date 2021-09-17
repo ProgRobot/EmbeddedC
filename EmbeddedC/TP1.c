79bcb55fec2367c9b4b9361d07dd3bca11a9dd68
YoucefKF <kfyoucef@gmail.com>
2021-09-17 23:27:58 +0200

    Améliorations 1 : AJout compteur, choix du mode du jeux

--- EmbeddedC/TP1.c
+++ EmbeddedC/TP1.c
@@ -19,40 +19,67 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 
 	//!Version 1: We have two players, the first write a secret number and the second try to find it
 
-	/*
-	double secretNumber = 0,rightNumber=0;
+	int continuerPartie = 1;
 
-	printf("PLAYER 1: Enter a secret number: ");
-	scanf_s("%f", &secretNumber);
-	
-	printf("PLAYER 1: I challenge you, find the secret number^^ \n");
+	while (continuerPartie != 2) {
 
-	do
-	{
-		printf("PLAYER 2: Enter the right number: ");
-		scanf_s("%f", &rightNumber);
+		int modeJeux = 0;
 
-		if (rightNumber == secretNumber)
+		printf("Choisir le mode de jeux:\n1-1 Joeur.\n2-2 Joeurs.\n");
+		scanf_s("%d",&modeJeux);
+
+		if (modeJeux == 1)
 		{
-			//TODO erreur à corriger pourquoi l'affichage du nombre ne se fait pas correctement ?
-			printf("Felicitation %f est le bon nombre",rightNumber);
-			break;
-		}
-		else if (rightNumber<secretNumber)
-		{
-			printf("The secret number is greater than the one you enter.\n");
+			double secretNumber = 0, rightNumber = 0;
+			int compteur = 0;
+
+			printf("PLAYER 1: Enter a secret number: ");
+			scanf_s("%f", &secretNumber);
+
+			printf("PLAYER 1: I challenge you, find the secret number^^ \n");
+
+			do
+			{
+				printf("PLAYER 2: Enter the right number: ");
+				scanf_s("%f", &rightNumber);
+				compteur++;
+				if (rightNumber == secretNumber)
+				{
+					//TODO erreur à corriger pourquoi l'affichage du nombre ne se fait pas correctement ?
+					printf("Felicitation %f est le bon nombre", rightNumber);
+					printf("\nVous l'avez trouve apres %d tentatives.", compteur);
+					break;
+				}
+				else if (rightNumber < secretNumber)
+				{
+					printf("The secret number is greater than the one you enter.\n");
+				}
+				else
+				{
+					printf("The secret number is fewer than the one you enter.\n");
+				}
+			} while (secretNumber != rightNumber);
+			printf("\nVoulez-vous refaire une autre partie ?\n1-Oui\n2-Non\n");
+			scanf_s("%d", &continuerPartie);
+			if (continuerPartie == 2)
+			{
+				break;
+			}
 		}
 		else
 		{
-			printf("The secret number is fewer than the one you enter.\n");
+			//todo ajouter mode jeux
 		}
-	} while (secretNumber!=rightNumber);
-	
-	*/
+
+	}
+    
+
+		
 
 	//!Version 2: We have one player, the computer generate a aleatory number. The Player have to find it.
-
+	//! 
 	//todo Correct warnings on the program
+	/*
 	double nombreMystere = 0;
 	const int MAX = 100, MIN = 1;
 
@@ -61,17 +88,19 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 
 	printf("COMPUTER: I chosed an aleatory number, find it !\n");
 
-	int playerNumber = 0;
+	int playerNumber = 0,compteur = 0;
 	
 	do
 	{
 		printf("PLAYER 2: Enter the right number: ");
 		scanf_s("%f", &playerNumber);
+		compteur++;
 
 		if (playerNumber == nombreMystere)
 		{
 			//TODO erreur à corriger pourquoi l'affichage du nombre ne se fait pas correctement ?
 			printf("Felicitation %f est le bon nombre", playerNumber);
+			printf("\nVous l'avez trouvé après &d tentatives.", compteur);
 			break;
 		}
 		else if (playerNumber < nombreMystere)
@@ -83,6 +112,6 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 			printf("The secret number is fewer than the one you enter.\n");
 		}
 	} while (nombreMystere != playerNumber);
-
+	*/
 	return 0;
 }
