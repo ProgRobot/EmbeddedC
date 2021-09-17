2f9e22bf62f0826ed57e11e8e116726062d60feb
YoucefKF <kfyoucef@gmail.com>
2021-09-17 16:23:27 +0200

    Adding the rand() function

--- EmbeddedC/TP1.c
+++ EmbeddedC/TP1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char* argv[]) {
 
@@ -18,12 +19,13 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 
 	//!Version 1: We have two players, the first write a secret number and the second try to find it
 
-	double secretNumber = 0, rightNumber=0;
+	/*
+	double secretNumber = 0,rightNumber=0;
 
 	printf("PLAYER 1: Enter a secret number: ");
 	scanf_s("%f", &secretNumber);
 	
-	printf("I challenge you, find the secret number^^ \n");
+	printf("PLAYER 1: I challenge you, find the secret number^^ \n");
 
 	do
 	{
@@ -33,7 +35,7 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 		if (rightNumber == secretNumber)
 		{
 			//TODO erreur à corriger pourquoi l'affichage du nombre ne se fait pas correctement ?
-			printf("Felicitation %f est le bon nombre", rightNumber);
+			printf("Felicitation %f est le bon nombre",rightNumber);
 			break;
 		}
 		else if (rightNumber<secretNumber)
@@ -45,9 +47,42 @@ Et ainsi de suite, jusqu'à ce que vous trouviez le nombre mystère.
 			printf("The secret number is fewer than the one you enter.\n");
 		}
 	} while (secretNumber!=rightNumber);
+	
+	*/
 
+	//!Version 2: We have one player, the computer generate a aleatory number. The Player have to find it.
+
+	//todo Correct warnings on the program
+	double nombreMystere = 0;
+	const int MAX = 100, MIN = 1;
+
+	srand(time(NULL));
+	nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+	printf("COMPUTER: I chosed an aleatory number, find it !\n");
+
+	int playerNumber = 0;
 	
-	
+	do
+	{
+		printf("PLAYER 2: Enter the right number: ");
+		scanf_s("%f", &playerNumber);
+
+		if (playerNumber == nombreMystere)
+		{
+			//TODO erreur à corriger pourquoi l'affichage du nombre ne se fait pas correctement ?
+			printf("Felicitation %f est le bon nombre", playerNumber);
+			break;
+		}
+		else if (playerNumber < nombreMystere)
+		{
+			printf("The secret number is greater than the one you enter.\n");
+		}
+		else
+		{
+			printf("The secret number is fewer than the one you enter.\n");
+		}
+	} while (nombreMystere != playerNumber);
+
 	return 0;
 }
-
